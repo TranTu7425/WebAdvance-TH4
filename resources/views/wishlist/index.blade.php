97f956c8a0677d8d3cf8208dca541bcf792f2581
TranTu7425 <23010332@st.phenikaa-uni.edu.vn>
2025-05-07 13:12:51 +0700

    update sign in and register

---
+++ resources/views/wishlist/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Wishlist</div>
+
+                <div class="card-body">
+                    Welcome to your wishlist!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\WishlistController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
+Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth', AuthAdmin::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
