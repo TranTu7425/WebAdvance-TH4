5844b6f81455a2a841de18956de3e1e8dc4b50f0
TranTu7425 <23010332@st.phenikaa-uni.edu.vn>
2025-06-08 16:54:08 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Slide;
 use App\Models\Category;
+use App\Models\Product;
 class HomeController extends Controller
 {    
     public function index()
     {
         $slides = Slide::where('status', '1')->get()->take(3);
         $categories = Category::orderBy('name')->get();
-        return view('index', compact('slides', 'categories'));
+        $salesProducts = Product::whereNotNull('sale_price')->orderBy('sale_price')->where('sale_price', '<>', '')->inRandomOrder()->get()->take(8);
+        return view('index', compact('slides', 'categories', 'salesProducts'));
     }
 }
